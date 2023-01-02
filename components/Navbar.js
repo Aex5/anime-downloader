34ed982965de4956b7aa0ceed796e5daf8cfc382
Aditya Arga <93075183+Sujaruu@users.noreply.github.com>
2023-01-02 16:00:07 +0000

    feat: add some styling and implement modal in search

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,12 +1,13 @@
-import { useState } from "react";
+import { useState, Fragment,useRef } from "react";
 import { MdSearch } from "react-icons/md";
 import Link from "next/link";
 
-import { Dialog } from "@headlessui/react";
+import { Dialog, Transition } from "@headlessui/react";
 
 export default function Navbar() {
   const [keyword, setKeyword] = useState("");
   const [isOpen, setIsOpen] = useState(false);
+  const inputRef = useRef(null);
 
   async function search() {
     window.location.href = `/search/${keyword}`;
@@ -24,30 +25,6 @@ export default function Navbar() {
   return (
     <div className="w-full bg-[#1c1c1c] fixed z-20 border-b-2 border-[#2e2e2e]">
       <nav className="max-w-[800px] mx-auto py-3">
-        <Dialog open={isOpen} onClose={() => setIsOpen(false)}>
-          <div className="fixed inset-0 bg-black/70 z-30 backdrop-blur-sm" aria-hidden="true">
-            <div className="fixed inset-0 flex items-center justify-center p-4 overflow-y-scroll">
-              <Dialog.Panel className="w-full max-w-sm rounded bg-[#2e2e2e]">
-                <Dialog.Title>search anime</Dialog.Title>
-                <Dialog.Description>ctrl + k</Dialog.Description>
-
-                <form onSubmit={handleSubmit} className="mb-5 text-slate-300">
-                  <input
-                    type="text"
-                    onChange={handleChange}
-                    value={keyword}
-                    placeholder="search anime"
-                    className="bg-[#1c1c1c] text-sm px-2 border-2 border-[#2e2e2e] rounded-md"
-                  />
-                  <button type="submit">
-                    <MdSearch />
-                  </button>
-                </form>
-              </Dialog.Panel>
-            </div>
-          </div>
-        </Dialog>
-
         <div className="flex justify-between items-center">
           <Link href="/">
             <a>
@@ -65,6 +42,53 @@ export default function Navbar() {
             <MdSearch />
           </button>
         </div>
+
+        <Transition
+          show={isOpen}
+          enter="transition-opacity duration-200 ease-in"
+          enterFrom="opacity-0"
+          enterTo="opacity-100"
+          leave="ease-out"
+          leaveFrom="translate-x-0 opacity-100"
+          leaveTo="translate-x-full opacity-0"
+          as={Fragment}
+        > 
+          <Dialog open={isOpen} initialFocus={inputRef} onClose={() => setIsOpen(false)}>
+            <div
+              className="fixed inset-0 bg-black/70 z-30 backdrop-blur-md"
+              aria-hidden="true"
+            >
+              <div className="fixed inset-0 flex items-center justify-center">
+                <Dialog.Panel className="w-full max-w-sm  bg-[#1e1e1e] rounded-xl p-10 border-2 border-[#2e2e2e] text-center">
+                  <Dialog.Title className="text-[#34b27b] font-semibold text-lg">
+                    Search Anime
+                  </Dialog.Title>
+                  <Dialog.Description className="text-sm text-slate-300 pb-5">
+                    Search All Batch Anime
+                  </Dialog.Description>
+
+                  <form
+                    onSubmit={handleSubmit}
+                    className="mb-5 text-slate-300 flex items-center justify-center gap-2"
+                  >
+                    <button type="submit">
+                      <MdSearch />
+                    </button>
+                    <input
+                      type="text"
+                      ref={inputRef}
+                      onChange={handleChange}
+                      value={keyword}
+                      placeholder="search anime"
+                      className="bg-[#1c1c1c] text-sm p-1 border-2 border-[#2e2e2e] rounded-md"
+                    />
+                     <button onClick={() => setIsOpen(false)} className='bg-[#2e2e2e] py-1 px-2 rounded-md'>esc</button>
+                  </form>
+                </Dialog.Panel>
+              </div>
+            </div>
+          </Dialog>
+        </Transition>
       </nav>
     </div>
   );
--- pages/search/[slug].js
+++ pages/search/[slug].js
@@ -1,6 +1,10 @@
 import { useState } from "react";
 import Link from "next/link";
 import Image from "next/image";
+import Layout from "../../components/Layout";
+
+import { motion } from "framer-motion";
+
 
 export async function getServerSideProps(ctx) {
   const name = ctx.query;
@@ -17,36 +21,48 @@ export async function getServerSideProps(ctx) {
   };
 }
 
+function limit(string = "", limit = 30) {
+  return string.substring(0, limit) + "...";
+}
+
 export default function Search({ data }) {
   return (
-    <div className="w-full text-center text-slate-200">
-      <div className="max-w-[800px] mx-auto">
-        <main className="grid grid-cols-2 gap-10">
-          {data.map((a, index) => {
-            return (
-              <div
-                key={index}
-                className="flex flex-col bg-[#282828] pb-10 rounded-lg"
-              >
-                <Image
-                  src={a.link.thumbnail}
-                  alt={a.title}
-                  width={200}
-                  height={200}
-                  className="rounded-t-lg"
-                />
-                <div>
-                  <h1 className="text-lg font-semibold">{a.title}</h1>
-                  <p className="text-slate-400">{a.release}</p>
-                </div>
-                <Link href={`/detailAnime/${a.link.endpoint}`}>
-                  <a>Detail</a>
-                </Link>
-              </div>
-            );
-          })}
-        </main>
+    <Layout>
+      <div className="w-full text-center text-slate-200 pt-28">
+      <motion.div
+          initial={{ y: 100, opacity: 0 }}
+          animate={{ opacity: 1, y: 0 }}
+          transition={{ duration: 0.5 }}
+        >
+        <div className="max-w-[800px] mx-auto">
+          <main className="grid grid-cols-3 gap-10">
+            {data.map((a, index) => {
+              return (
+                <Link href={`/detailAnime/${a.link.endpoint}`} key={index}>
+                <a>
+                  <div className="h-56 flex flex-col bg-[#282828] pb-5 rounded-lg">
+                    <Image
+                      src={a.link.thumbnail}
+                      alt={a.title}
+                      width={300}
+                      height={150}
+                      className="rounded-t-lg object-cover"
+                    />
+                    <div className="mt-2">
+                      <h1 className="text-md text-slate-300 font-semibold px-2">
+                        {limit(a.title)}
+                      </h1>
+                      <p className="text-[#34b27b] text-xs">{a.release}</p>
+                    </div>
+                  </div>
+                </a>
+              </Link>
+              );
+            })}
+          </main>
+        </div>
+        </motion.div>
       </div>
-    </div>
+    </Layout>
   );
 }
